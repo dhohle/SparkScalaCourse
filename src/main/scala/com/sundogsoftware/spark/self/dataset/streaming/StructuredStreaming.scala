5462fa02c9b63c924c2232d67ce33a8a9e446cc1
Daniël Höhle <daniel.hohle@gmail.com>
2021-03-30 13:54:24 +0200

    minor update, small problem in regex

--- src/main/scala/com/sundogsoftware/spark/self/dataset/streaming/StructuredStreaming.scala
+++ src/main/scala/com/sundogsoftware/spark/self/dataset/streaming/StructuredStreaming.scala
@@ -23,8 +23,8 @@ object StructuredStreaming {
     // Regular Expressions to extract pieces of Apache Access log lines
     val contentSizeExp = "\\s(\\d+)$"
     val statusExp = "\\s(\\d{3})\\s"
-    val generalExp = "\"(\\S+)\\s(\\S)\\s*(\\S+)\""
-    val timeExp = "\\[(\\d{2}/\\w{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} - \\d{4})]"
+    val generalExp = "\"(\\S+)\\s(\\S+)\\s*(\\S*)\""
+    val timeExp = "\\[(\\d{2}/\\w{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} -\\d{4})]"
     val hostExp = "(^\\S+\\.[\\S+\\.]+\\S+)\\s"
 
     // Apply these regular expressions to create structure from the unstructured text
